0c936b56446517a29c95ef44be167b661a1a61ec
montuyajoel <2025040@student.cct.ie>
2025-07-15 23:33:06 +0800

    fix: improve workflow

--- api/models.py
+++ api/models.py
@@ -4,5 +4,4 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     author = models.CharField(max_length=255)
-    new_field = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
