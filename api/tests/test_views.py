5f9e4def72590b9a5d1a6a634fe3ff30b70cfc6a
montuyajoel <2025040@student.cct.ie>
2025-07-15 19:08:25 +0800

    feat: enabling post endpoint for book creation

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,15 +1,34 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from api.models import Book
+
 
 class BookViewTest(APITestCase):
     
     def test_response_is_correct(self):
+        book = Book.objects.create(
+            **{
+                "title": "test_title",
+                "description": "test_description",
+                "author": "test_author"
+            }
+        )
+
         url = reverse('api:books')
         response = self.client.get(url, format='json')
         assert response.status_code == status.HTTP_200_OK
         body = response.json()
-        assert body['hello'] == 'django'
+        assert body== [          
+            {
+                "title": book.title,
+                "description": book.description,
+                "author": book.author,
+                "created_at": book.created_at.isoformat().replace("+00:00", "Z")               
+            }
+        ]
+
+    
 
 class HealthViewTest(APITestCase):
     
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import status
 
-# Create your views here.
 class HealthView(APIView):
     def get(self, request, *args, **kwargs):
         return Response({
@@ -11,13 +12,30 @@ class HealthView(APIView):
 
 health_view = HealthView.as_view()
 
+#
+# /api/books - All method (GET, POST, UPDATE, DELETE)
+#
 
 class BookView(APIView):
     def get(self, request, *arg, **kwargs):
-        return Response(
-            {
-                'hello':'django'
-            }
-        )
+        all_books = Book.objects.all()
+        serializers = BookSerializer(all_books, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, *args, **kwargs):
+        data = request.data
+
+        serializer = BookSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        #### Another approach for handling incorrect format
+        #  if not serializer.is_valid():
+        #  return Response('The data is invalid.', status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({
+            'status':'ok',
+            'data':serializer.data
+            })
+    
     
 book_view = BookView.as_view()
